079efab1d1807bdbc6dc385d37921fb23e48c312
armand.foucault@telecom-bretagne.eu <armand@armand-laptop.localhost>
2017-12-19 10:48:47 +0100

    Made the typing_policy decorator chainable with other decorators
    - The typing_policy decorator no longer returns a wrapper object, but
      returns a function: it can now be chained with other decorators
    - The type checks are performed inside of the decorated function

--- src/decorators.py
+++ src/decorators.py
@@ -1,7 +1,17 @@
-from .typed_function import TypedFunction
+from .type_checker import TypeChecker
 
 
 def typing_policy(policy):
     def typing_policy_decorator(function):
-        return TypedFunction(function, policy)
+        typeChecker = TypeChecker(function, policy)
+
+        def typing_policy_decorated(*args, **kwargs):
+            args = list(args)
+            typeChecker.process_arguments(args, kwargs)
+            result = typeChecker.process_result(function(*args, **kwargs))
+
+            return result
+
+        return typing_policy_decorated
+
     return typing_policy_decorator
