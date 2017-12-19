75bbe71aab45f2732b577d4cfafde13b0d9244c6
armand.foucault@telecom-bretagne.eu <armand@armand-laptop.localhost>
2017-12-19 10:57:26 +0100

    Made typing_policy return TypedFunction.__call__
    - The typing_policy decorator creates a TypedFunction instance, but now
      returns the __call__ method of the latter, rather than the latter
      itself. Thus, the decorator now returns a function object, that can
      once again be decorated. Therefore, the typing_policy decorator is now
      chainable.

--- src/decorators.py
+++ src/decorators.py
@@ -3,5 +3,5 @@ from .typed_function import TypedFunction
 
 def typing_policy(policy):
     def typing_policy_decorator(function):
-        return TypedFunction(function, policy)
+        return TypedFunction(function, policy).__call__
     return typing_policy_decorator
