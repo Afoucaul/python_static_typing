9a4b83c90a4fee21eb43584a18367aa996438ae8
armand <armand.foucault@telecom-bretagne.eu>
2018-04-11 00:31:58 +0200

    Added two shorthand decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,7 +1,12 @@
 from .typed_function import TypedFunction
+from .policy import CAST, STRICT
 
 
 def typing_policy(policy):
     def typing_policy_decorator(function):
         return TypedFunction(function, policy).__call__
     return typing_policy_decorator
+
+
+weakly_typed = typing_policy(CAST)
+strongly_typed = typing_policy(STRICT)
